31b37722dbda6ac744949f3287eaa024448474b3
modares <modareshadi@gmail.com>
2020-08-18 09:26:00 -0700

    done with day 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,41 +2,67 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.PC = 0
+        self.IR = []
 
-    def load(self):
+    def ram_read(address: int):
+        return self.ram[address]
+
+    def ram_read(value, address: int):
+        self.ram[address] = value
+
+    def load(self, file_dir):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010,  # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111,  # PRN R0
+        #     0b00000000,
+        #     0b00000001,  # HLT
+        # ]
+        program = []
+        with open(file_dir) as file_obj:
+            program = file_obj.readlines()
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
+    def LDI(self):
+        reg_num = self.ram[self.PC + 1]
+        self.PC += 1
+        value = self.ram[self.PC + 2]
+        self.PC += 1
+        self.ram[reg_num] = value
+
+    def PRN(self):
+        reg_num = self.ram[self.PC + 1]
+        self.PC += 1
+        print(self.ram[reg_num])
+
+    def HLT(self):
+        exit()
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +74,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +88,21 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+        while running:
+            ir = self.ram[self.PC]
+
+            if ir == 0b10000010:
+                self.LDI()
+
+                self.PC += 1
+
+            if ir == 0b01000111:
+                self.PRN()
+
+                self.PC += 1
+
+            if ir == 0b00000001:
+                self.HLT()
+
+                self.PC += 1
